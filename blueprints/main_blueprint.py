724aeaafad2cece120bcd1ca64c9349d30aac420
joe_ee <joeyskimboard@gmail.com>
2023-11-22 18:53:08 -0800

    more changes to ensure it works with new code

--- blueprints/main_blueprint.py
+++ blueprints/main_blueprint.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, session, flash, render_template
+from flask import Blueprint, request, redirect, session, flash, render_template, jsonify
 
 from models.models import db, User
 
@@ -8,28 +8,32 @@ router = Blueprint('main', __name__, template_folder='templates')
 def default():
     return redirect('/login')
 
-@router.get('/create_account')
-def create_account_page():
-    return render_template('accountcreation.html')
-
-@router.post('/create_account')
+@router.route('/create_account', methods=['GET', 'POST'])
 def create_account():
-    username = request.form['username']
-    name = request.form['name']
-    email = request.form['email']
-    imagecategory = request.form['imagecategory']
+    session['redire'] = '/create2'
+    session['flag'] = 0
+    if request.method == 'POST':
+        username = request.form['username']
+        name = request.form['name']
+        email = request.form['email']
+        imagecategory = request.form['imagecategory']
+        print("HEY")
+        user = User(username=username, name=name, email=email, imagecategory=imagecategory)
+        print("HEY")
+        session['currentUser'] = user.username
+        print("HEY")
+        db.session.add(user)
+        db.session.commit()
 
-    user = User(username=username, name=name, email=email, imagecategory=imagecategory)
-    session['currentUser'] = user.username
-    db.session.add(user)
-    db.session.commit()
-
-    flash("Account Information taken")
-    return redirect("/create2")
+        flash("Account Information taken")
+        return redirect("/create2")
+    return render_template('accountcreation.html')
 
 @router.get('/login')
 def login_page():
+    session['corVal'] = 0
     return render_template('login.html')
+
 
 @router.post('/login')
 def login():
@@ -38,7 +42,7 @@ def login():
     user = User.query.filter_by(username=username).first()
     #if username is good then go to next step
     if user:
-        session['user'] = user.username
+        session['username'] = user.username
 
         #If the username is good then we go to the next step in the login process which is image upload for face recognition
     return redirect("/upload", code=302)
